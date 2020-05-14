c69ddbb804ccbb0355ed41c0d811020da32c01d9
kytewy <wyattkyte@hotmail.com>
2020-05-13 22:02:44 -0400

    Adding comparison operators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -17,6 +17,39 @@ const courseSchema = new mongoose.Schema({
 // Creating a class
 const Course = mongoose.model('Course', courseSchema);
 
-const course = new Course({
+async function createCourse() {
+    const course = new Course({
+        name: "Python Course",
+        author: "Wyatt",
+        tags: [ "learning", "Data Science", "back-end"],
+        isPublished: true
+    });
 
-});
+    const result = await course.save()
+    console.log(result)
+}
+
+async function getCourses() {
+    // comparison operators 
+    // eq (not equal) 
+    // ne (not equal)
+    // gt (greater than)
+    // gte (greater than or equal to)
+    // lt (less than)
+    // lte (less than or equal to)
+    // in 
+    // nin (not in)
+
+// FInding something greater than 10 and less than 20
+// find({ price: {$gte: 10, $lte:20 }})
+// find({ price: {$in: [10, 15, 20] }})
+
+    const courses = await Course
+    .find( {tags: 'Data Science'})
+    .limit(10)
+    .sort( {name : 1 })
+    .select( {name : 1, tags: 1});
+    console.log(courses)
+} 
+
+getCourses();
